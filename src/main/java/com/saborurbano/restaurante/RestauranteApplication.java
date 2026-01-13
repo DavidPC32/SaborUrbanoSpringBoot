38b02b22e897ab7aa2e75c0186e1b83e7420a836
evaluacion <jolfre.gonzalez01@gamil.com>
2026-01-13 03:20:37 -0500

    Solucion a nombre de calificaion que impedia el arranque de la api

--- src/main/java/com/saborurbano/restaurante/RestauranteApplication.java
+++ src/main/java/com/saborurbano/restaurante/RestauranteApplication.java
@@ -2,9 +2,6 @@ package com.saborurbano.restaurante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.saborurbano.restaurante.model.Usuarios;
-import com.saborurbano.restaurante.repository.UsuariosRepository;
-import com.saborurbano.restaurante.service.Usuario.UsuarioServiceImp;
 
 
 @SpringBootApplication
--- src/main/java/com/saborurbano/restaurante/model/CalificacionPlatillo.java
+++ src/main/java/com/saborurbano/restaurante/model/CalificacionPlatillo.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
-@Table(name = "Calificacion_platillo")
+@Table(name = "Calificacion")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
