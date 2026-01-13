52297f1166048dd48d0c226406202e07b750477a
Omar <salcedosolarteomar@gmail.com>
2026-01-13 03:48:27 -0500

    Integración del swagger para la documentación de la api

---
+++ src/main/java/com/saborurbano/restaurante/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.saborurbano.restaurante.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Restaurante Sabor Urbano", version = "1.0.0", 
+description = "Documentación para el API de Restaurante Sabor Urbano"))
+
+public class OpenApiConfig {
+    
+}
--- src/main/java/com/saborurbano/restaurante/controller/UsuariosController.java
+++ src/main/java/com/saborurbano/restaurante/controller/UsuariosController.java
@@ -14,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.saborurbano.restaurante.model.Usuarios;
 import com.saborurbano.restaurante.service.Usuario.UsuarioServiceImp;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RequestMapping("api/usuarios")
 @RestController
+@Tag(name = "Usuarios", description = "Endpoint para gestionar usuarios")
 public class UsuariosController {
 
     private final UsuarioServiceImp usuarioServiceImp;
@@ -26,17 +32,37 @@ public class UsuariosController {
 
     }
 
+    @Operation(summary = "Obtener todos los usuarios", description = "Devuelve una lista de usuarios")
+    @ApiResponses(value ={
+        @ApiResponse(responseCode = "200", description = "Devuelve una lista de usuarios"),
+        @ApiResponse(responseCode = "400", description = "Error del cliente"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @GetMapping
     public List<Usuarios> getAllUsuarios() {
         return usuarioServiceImp.getAllUsuarios();
     }
 
+
+
+    @Operation(summary = "Crear un usuario", description = "Crea un nuevo usuario")
+    @ApiResponses(value ={
+        @ApiResponse(responseCode = "201", description = "Usuario creado correctamente"),
+        @ApiResponse(responseCode = "400", description = "Error del cliente no se puedo crear el usuario"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @PostMapping
     public ResponseEntity<Usuarios> createUsuario(@RequestBody Usuarios usuarios) {
         Usuarios  nuevoUsuario = usuarioServiceImp.registrarUsuarios(usuarios);
         return new ResponseEntity<>(nuevoUsuario, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Eliminar un usuario", description = "Elimina un usuario")
+    @ApiResponses(value ={
+        @ApiResponse(responseCode = "204", description = "Usuario eliminado correctamente"),
+        @ApiResponse(responseCode = "400", description = "Error del cliente no se puede eliminar el usuario"),
+        @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUsuarios(@PathVariable Integer id, Usuarios usuarios) {
         usuarioServiceImp.deleteUsuarios(id, usuarios);
